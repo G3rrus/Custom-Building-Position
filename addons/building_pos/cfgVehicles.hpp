90e48ef932fae1c47d40a7e843537d12145fcdce
G3rrus <daniel_schreiner@live.at>
2019-04-28 19:46:04 +0200

    formatting changes

---
+++ addons/building_pos/cfgVehicles.hpp
@@ -0,0 +1,11 @@
+class CfgVehicles {
+	class Strategic;
+	class Land_G4rrus_BuildingPos_F: Strategic {
+		displayName = "Building Position";
+		scope = 2;
+		scopeArsenal = 0;
+		model = "\buildingPos.p3d"; // not yet right path
+		editorCategory = "EdCat_Structures_Altis";
+		editorSubcategory = "EdSubcat_Military";
+	};
+};

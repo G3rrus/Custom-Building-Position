dbef2bd6a715a1b4c6c99a39e2f5668d4b0a61aa
G3rrus <daniel_schreiner@live.at>
2019-04-28 19:33:14 +0200

    First real commit

---
+++ addons/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches {
+	class g4rrus_buildingpos {
+		name = "Custom Building Position";
+		units[] = {"Land_G4rrus_BuildingPos_F"};
+		weapons[] = {};
+		requiredVersion = 1.82;
+		requiredAddons[] = {"A3_Structures_F_Mil"};
+		authors[] = {"G4rrus","Mallow"};
+		authorUrl = "https://github.com/G3rrus";
+		version = "1.0.0";
+		versionStr = "1.0.0";
+		license = "https://www.bohemia.net/community/licenses/arma-public-license-share-alike";
+	};
+};
+
+#include "cfgVehicles.hpp"
